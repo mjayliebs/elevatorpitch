a8c455cfc52b23b1f6b75bce312dbd23cdd7e604
Mitch Lieberman <mlieberman@sugarcrm.com>
2023-02-08 13:22:31 -0500

    Tweak 6

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,7 +69,7 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2">
       <Head>
         <title>Elevator Pitch Generator</title>
         <link rel="icon" href="/favicon.ico" />
